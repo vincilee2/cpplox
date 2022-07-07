f99183b134f93af8d7466f455497bdd825a7fafd
vincilee <vincilee1994@outlook.com>
2022-07-07 22:46:48 +0800

    add tokenType

---
+++ TokenType.hpp
@@ -0,0 +1,42 @@
+#ifndef TOKEN_TYPE_HPP
+#define TOKEN_TYPE_HPP
+
+#include<string>
+#include<iostream>
+
+namespace cpplox:Types {
+    enum class TokenType {
+        // Single-character tokens
+        LEFT_PAREN,
+        RIGHT_PAREN,
+        LEFT_BRACE,
+        RIGHT_BRACE,
+        COLON,
+        COMMA,
+        DOT,
+        QUESTION,
+        SEMICOLON,
+        SLASH,
+        STAR,
+
+        // One or two character tokens
+        BANG,
+        BANG_EQUAL,
+        EQUAL,
+        EQUAL_EQUAL,
+        GREATER,
+        GREATER_EQUAL,
+        LESS,
+        LESS_EQUAL,
+
+        // Literals
+        IDENTIFIER,
+        STRING,
+        NUMBER,
+
+        // Keywords
+        AND, CLASS, ELSE, FALSE, FUN, FOR, IF, NIL, OR,
+        PRINT, RETURN, SUPER, THIS, TRUE, VAR, WHILE,
+
+    }
+}
